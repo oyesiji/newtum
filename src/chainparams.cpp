538b4958fc7f4091c47b1200106f3225c251b615
Jubril Siji <oyesiji@gmail.com>
2021-05-16 04:13:11 -0400

    made changes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -122,8 +122,8 @@ public:
         // Note that of those with the service bits flag, most only support a subset of possible options
         vSeeds.push_back(CDNSSeedData("192.168.162.137", "192.168.162.137", true));
        // vSeeds.push_back(CDNSSeedData("thrasher.io", "dnsseed.thrasher.io", true));
-       // vSeeds.push_back(CDNSSeedData("litecointools.com", "dnsseed.litecointools.com"));
-       // vSeeds.push_back(CDNSSeedData("litecoinpool.org", "dnsseed.litecoinpool.org"));
+       // vSeeds.push_back(CDNSSeedData("newtumcointools.com", "dnsseed.newtumcointools.com"));
+       // vSeeds.push_back(CDNSSeedData("newtumcoinpool.org", "dnsseed.newtumcoinpool.org"));
        // vSeeds.push_back(CDNSSeedData("koin-project.com", "dnsseed.koin-project.com"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,53);
@@ -226,7 +226,7 @@ public:
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
         vSeeds.push_back(CDNSSeedData("192.168.162.137", "192.168.162.137"));
-        //vSeeds.push_back(CDNSSeedData("loshan.co.uk", "seed-b.litecoin.loshan.co.uk", true));
+        //vSeeds.push_back(CDNSSeedData("loshan.co.uk", "seed-b.newtumcoin.loshan.co.uk", true));
        // vSeeds.push_back(CDNSSeedData("thrasher.io", "dnsseed-testnet.thrasher.io", true));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
